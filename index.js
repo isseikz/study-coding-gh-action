14c5a7916f9304dadf3c06532521264025fd6312
isseikz <issey.macky@gmail.com>
2022-08-11 00:22:47 +0900

    tmp

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ async function createBranchWithPatch(id, patch) {
     const replacedPatch = patch + "\n";
     console.log(replacedPatch);
     fs.writeFileSync("../diff.patch", replacedPatch);
-    await exec(`patch < ../diff.patch`);
+    await exec(`patch -p1 < ../diff.patch`);
     await exec(`git add . `);
     await exec(`git commit -m "Test"`);
     return branchName;
